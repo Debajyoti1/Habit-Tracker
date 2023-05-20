cf49c94b90f6f1c5b80f84bb9ac6f3ca88268785
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-05-20 07:58:09 +0530

    corrected function

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController');
 router.use('/', require('./habit'));
 module.exports = router;
